633fcbfea6140b6d6dadc7848a6a0da34568dd0f
Elina <elina.kudiyarova@gmail.com>
2019-03-31 12:27:06 +0300

    hw

---
+++ 2. Functions/Homework.js
@@ -0,0 +1,19 @@
+function print(x){
+    console.log(x);
+}
+
+function square(p){
+    return p*p;
+}
+
+print (square(25) + square(15));
+
+function multiply(a, c){
+    return 4*a*c
+}
+
+print (square(1) + multiply(2, 2))
+
+print (square(4) + multiply(4, 4))
+
+print (square(5) + multiply(8, 9))
--- 2. Functions/Lessons.js
+++
@@ -1,9 +0,0 @@
-function print(x){
-    console.log(x);
-}
-
-function square(p){
-    return p*p;
-}
-
-print(square(5) + square(10));
