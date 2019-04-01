efbe59f897e52aae62715cfb4f40707bf5cdd469
Elina <elina.kudiyarova@gmail.com>
2019-04-01 11:14:36 +0300

    homework

--- 2. Functions/Homework.js
+++ 2. Functions/Homework.js
@@ -8,12 +8,12 @@ function square(p){
 
 print (square(25) + square(15));
 
-function multiply(a, c){
-    return 4*a*c
+function des(a, b, c){
+    return b*b + 4*a*c
 }
 
-print (square(1) + multiply(2, 2))
+console.log (des(1, 2, 2))
 
-print (square(4) + multiply(4, 4))
+console.log (des(3, 1, 6))
 
-print (square(5) + multiply(8, 9))
+console.log (des(4, 5, 4))
