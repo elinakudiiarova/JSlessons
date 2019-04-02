6c2d48edea2b947a40534488c011be5dd117d3b4
Elina <elina.kudiyarova@gmail.com>
2019-04-02 12:57:01 +0300

    lesson

---
+++ 5. Conditional operator/Lesson.js
@@ -0,0 +1,28 @@
+function des(a, b, c) {
+    return b * b - 4 * a * c;
+}
+
+function solve(o, p, s) {
+    const d = des(o, p, s);
+    console.log(d);
+    if (d < 0) {
+        console.log(`Дискриминант равен ${d}. Дискриминант меньше нуля, значит корней нету.`);
+    }
+
+    if (d === 0) {
+        const root = -p / (2 * o);
+        console.log(`Дискриминант равен 0, значит есть один корень равен ${root}`);
+    }
+
+    if(d > 0) {
+        const root1 = (-p + Math.sqrt(d)) / (2*o);
+        const root2 = (-p - Math.sqrt(d)) / (2*o);
+        console.log(`Дискриминант равен ${d}, значит есть два корня: ${root1} и ${root2}`);
+    }
+}
+
+solve(2, 4, 5);
+
+solve(2, 4, 2);
+
+solve(2, 10, 1);
