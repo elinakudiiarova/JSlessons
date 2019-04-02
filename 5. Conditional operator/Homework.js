6ef84d34b3aa323f45939377c580c9f0c2ad689b
Elina <elina.kudiyarova@gmail.com>
2019-04-02 19:46:22 +0300

    changes

--- 5. Conditional operator/Homework.js
+++ 5. Conditional operator/Homework.js
@@ -1,18 +1,25 @@
 function add(a, b, c) {
     return (a + b + c) / 2;
 }
-    
+
+function isValidTriangleSides(m, h, s) {
+    return (m < (h + s)) && (h < (m + s)) && (s < (m + h));
+}
+
 function solve(t, g, l) {
     const p = add(t, g, l);
     console.log(p);
     if (t > 0 && g > 0 && l > 0 && p > 0) {
-    const square = Math.sqrt(p*(p - t)*(p - g)*(p - l));
-    console.log(`Площадь треугольника равна ${square} см^2`);
+        if (isValidTriangleSides(t, g, l)) {
+            const square = Math.sqrt(p * (p - t) * (p - g) * (p - l));
+            console.log(`Площадь треугольника равна ${square} см^2`);  
+        } else {
+            console.log(`Площадь не может быть вычеслена.`)
+        }
+    } else {
+        console.log(`Площадь треугольника равна 0.Её нельзя вычислить.`);
     }
 
-    if (t < 0 || g < 0 || l < 0 || p < 0) {
-    console.log(`Площадь треугольника равна 0.Её нельзя вычислить.`);
-    }
 }
 
 solve(3, 4, 5);
