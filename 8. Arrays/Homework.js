7194fbca9fcb60f0299962e97277b635f1e63998
Elina <elina.kudiyarova@gmail.com>
2019-04-19 07:43:25 +0300

    1

---
+++ 8. Arrays/Homework.js
@@ -0,0 +1,33 @@
+const c = [{
+    name: `Elya`,
+    marks: [10, 9, 10, 11, 12, 10, 9, 8, 10]
+}, {
+    name: `Nicole`,
+    marks: [9, 11, 8, 8, 10, 11, 10, 7, 11]
+}, {
+    name: `Katya`,
+    marks: [7, 9, 10 , 8, 5, 8, 9, 10]
+}, {
+    name: 'Alina',
+    marks: [4, 8, 5, 11, 7, 8, 5, 11]
+    
+}, {
+    name: `Ilya`,
+    marks: [10, 9, 6, 7, 10 , 8, 9, 5]
+}, {
+    name: `Vlad`,
+    marks: [5, 8, 9, 11, 7, 9, 9, 8, 10]
+}, {
+    name: `Arina`,
+    marks: [10, 10, 11, 11, 8, 7, 9, 10]
+}, {
+    name: `Lisa`,
+    marks: [10, 11, 7, 9, 9, 10, 10, 9]
+}, {
+    name: `Masha`,
+    marks: [5, 7, 10, 11, 6, 7, 8, 9, 6]
+}, {
+    name: `Leonid`,
+    marks: [10, 6, 8, 4, 10, 5, 7, 6 , 9, 9]
+}];
+console.log(c);
