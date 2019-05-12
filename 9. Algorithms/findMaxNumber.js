3b38eb49d4ad80ce15bd30de9738281d050ad9ae
Elina Kudiiarova <elina.kudiyarova@gmail.com>
2019-05-12 17:29:47 +0300

    index

--- 9. Algorithms/findMaxNumber.js
+++ 9. Algorithms/findMaxNumber.js
@@ -10,4 +10,17 @@ for (let i = 0; i < arr.length; i++) {
 
 console.log(max)
 
+// find index of max number in array
+
+let max = arr[0]; let maxi = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+        maxi = i;
+    };
+
+};
+
+console.log(max)
+console.log(maxi)
 
