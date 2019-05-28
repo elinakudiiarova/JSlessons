dde4881ff764f16210bbae7f6ca230a21877d23e
Elina Kudiiarova <elina.kudiyarova@gmail.com>
2019-05-28 23:58:09 +0300

    cvbn

--- 11. Methods of arrays/homework.js
+++ 11. Methods of arrays/homework.js
@@ -6,6 +6,6 @@ console.log(array.reduce((array, elem) => array + elem));
 
 console.log(array.reduce((array, elem) => array + elem) / array.length);
 
-const word = [`Lena`, `Lika`, `Lisa`, `Lisa`];
+const word = [`Lena,`, `Lika,`, `Lisa,`, `Lisa`];
 
 console.log(word.reduce((word, sum) => word + sum));
