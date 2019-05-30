f26fe6dbc713b683d2e8fd74ba354952f096105e
Elina Kudiiarova <elina.kudiyarova@gmail.com>
2019-05-30 13:58:23 +0300

    reduce

--- 11. Methods of arrays/homework.js
+++ 11. Methods of arrays/homework.js
@@ -2,10 +2,21 @@
 
 const array = [20, 10, 3, 7];
 
-console.log(array.reduce((array, elem) => array + elem));
+console.log(array.reduce((result1, elem) => result1 + elem));
 
-console.log(array.reduce((array, elem) => array + elem) / array.length);
+console.log(array.reduce((result, elem, index) => {
+    console.log(`Iteration: ${index}, element: ${elem}, result: ${result}`);
+    return result + elem / array.length;
+}, 0));
 
 const word = [`Lena,`, `Lika,`, `Lisa,`, `Lisa`];
 
 console.log(word.reduce((word, sum) => word + sum));
+
+console.log(array.reduce((min, elem) => {
+    if (elem < min) {
+        return elem;
+    } else {
+        return min;
+    };
+}));
