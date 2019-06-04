67e129bf09b7cf7763e5c8e7cc9a65860e248769
Elina Kudiiarova <elina.kudiyarova@gmail.com>
2019-06-04 12:34:47 +0300

    export.import

---
+++ 13. Export and import/calc.js
@@ -0,0 +1,3 @@
+const math = require("./math.js");
+console.log(math.pi);
+console.log(math.addnumbers(5,4));
---
+++ 13. Export and import/math.js
@@ -0,0 +1,6 @@
+const pi = 3.14;
+function add(x, y){
+    return x + y;
+}
+module.exports.pi = pi;
+module.exports.addnumbers = add;
